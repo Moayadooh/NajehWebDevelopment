15296756d30da006d4f7252919593d68b2f7e546
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-10-01 16:37:04 +0400

    Commit

--- Practical_Project_1/Practical_Project_1/js/custom.js
+++ Practical_Project_1/Practical_Project_1/js/custom.js
@@ -1,1 +1,27 @@
 ﻿
+
+$(function () {
+    $("header figure.burger-image").animate({ left: "0%", opacity: 1 }, 300);
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 200) {
+            $("#best-burger figure.popular, #best-burger figure.fun, #best-burger figure.fresh").animate({ left: "0%", opacity: 1 }, 1000);
+        }
+        if ($(window).scrollTop() >= 800) {
+            $("#choose-burger figure.left-burger, #choose-burger figure.right-burger").animate({ left: "0%", opacity: 1 }, 1000);
+            $("#choose-burger figure.mid-burger").animate({ opacity: 1 }, 1000);
+        }
+        if ($(window).scrollTop() >= 1800) {
+            $("#reservation div.food-images").animate({ opacity: 1 }, 1000);
+        }
+    });
+
+    //$("#choose-burger figure.mid-burger img").mouseenter(function () {
+    //    $(this).animate({ width: "90%"}, 500);
+    //});
+
+    //$("#choose-burger figure.mid-burger img").mouseleave(function () {
+    //    $(this).animate({ width: "100%" }, 500);
+    //});
+    
+});
