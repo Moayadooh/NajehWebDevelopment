c798a94c1c83c69a50b8dae322ae089cf4883b26
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-10-03 20:23:28 +0400

    The project is done!

--- Practical_Project_1/Practical_Project_1/js/custom.js
+++ Practical_Project_1/Practical_Project_1/js/custom.js
@@ -40,111 +40,19 @@ $(function () {
 
     //Login Form
     $("#login").click(function () {
-        //$("header .sign-in-form").animate({ opacity: 0, position: "relative", left: "900%" }, 0, function () {
-        //    $(this).animate({ opacity: 1, position: "relative", left: 0, top: 0 }, 500);
-        //});
-        $("header .sign-in-form").animate({ opacity: 1, position: "relative", left: 0 }, 0);
+        $("header .form-login").modal('show');
+        $("header .form-login").animate({ opacity: 1, left: "50%" }, 350);
     });
-    $(".form-signin .glyphicon-remove").click(function () {
-        $("header .sign-in-form").animate({ opacity: 0, position: "relative", left: "-300%" }, 200, function () {
-            $(".modal").modal('hide');
+
+    $("header .form-login .glyphicon-remove").click(function () {
+        $("header .form-login").animate({ opacity: 0, left: "-50%" }, 350, function () {
+            $(this).animate({ opacity: 0, left: "200%" }, 0, function () {
+                $(this).modal('hide');
+            });
         });
     });
 
     //Navigation Bar
-    function WhiteHover() {
-        $("header nav a").css({ color: "rgb(61, 37, 20)" });
-        $("header nav a").mouseenter(function () {
-            $(this).css({ color: "#ffffff" });
-        });
-        $("header nav a").mouseleave(function () {
-            $(this).css({ color: "rgb(61, 37, 20)" });
-        });
-    }
-
-    function WhiteHover2() {
-        $("header nav a").css({ color: "#000000" });
-        $("header nav a").mouseenter(function () {
-            $(this).css({ color: "#ffffff" });
-        });
-        $("header nav a").mouseleave(function () {
-            $(this).css({ color: "#000000" });
-        });
-    }
-
-    function GrayHover() {
-        $("header nav a").css({ color: "#ffffff" });
-        $("header nav a").mouseenter(function () {
-            $(this).css({ color: "darkgray" });
-        });
-        $("header nav a").mouseleave(function () {
-            $(this).css({ color: "#ffffff" });
-        });
-    }
-
-    var isMobile = false;
-    var isTablet = false;
-    var isLaptop = false;
-    function CheckDeviceWidth() {
-        isMobile = false;
-        isTablet = false;
-        isLaptop = false;
-        if ($(window).width() < 768) {
-            isMobile = true;
-        }
-        else if ($(window).width() >= 768 && $(window).width() <= 991) {
-            isTablet = true;
-        }
-        else {
-            isLaptop = true;
-        }
-    }
-
-    function NavbarFormat() {
-        if (isMobile) {
-            $(".navigation").addClass("navbar-fixed-top");
-            GrayHover();
-        }
-        else if (isTablet) {
-            $(".navigation").removeClass("navbar-fixed-top");
-            FixNavbar();
-        }
-        else {
-            $(".navigation").removeClass("navbar-fixed-top");
-            if ($(window).scrollTop() >= 5) {
-                FixNavbar();
-            }
-        }
-    }
-
-    function FixNavbar() {
-        $("header nav").css({
-            position: "fixed",
-            left: 0,
-            top: -22,
-            backgroundColor: "darkgray",
-            opacity: 0.5,
-            padding: "7px",
-            width: "100%",
-            zIndex: 3
-        });
-        WhiteHover2();
-    }
-
-    function FreeNavbar() {
-        $("header nav").css({
-            width: "auto",
-            background: "none",
-            opacity: 1,
-            marginTop: "21px",
-            marginRight: "46px",
-            position: "relative",
-            left: 0,
-            top: 0
-        });
-        WhiteHover();
-    }
-
     CheckDeviceWidth();
     NavbarFormat();
     $(window).resize(function () {
@@ -168,7 +76,7 @@ $(function () {
             else if ($(window).scrollTop() < 5) {
                 FreeNavbar();
             }
-            //Images Animation
+            //Images Animation (Laptop)
             if ($(window).scrollTop() >= 200) {
                 $("#best-burger figure.popular, #best-burger figure.fun, #best-burger figure.fresh").animate({ left: "0%", opacity: 1 }, 1000);
             }
@@ -181,8 +89,22 @@ $(function () {
             }
         }
         else if (isTablet) {
-            //Images Animation
+            //Images Animation (Tablet)
             if ($(window).scrollTop() >= 100) {
+                $("header figure.burger-image").animate({ left: "0%", opacity: 1 }, 300);
+            }
+            if ($(window).scrollTop() >= 300) {
+                $("#best-burger figure.popular, #best-burger figure.fun, #best-burger figure.fresh").animate({ left: "0%", opacity: 1 }, 1000);
+            }
+            $("#choose-burger figure.left-burger, #choose-burger figure.right-burger").animate({ left: "0%", opacity: 1 }, 1000);
+            $("#choose-burger figure.mid-burger").animate({ opacity: 1 }, 1000);
+            if ($(window).scrollTop() >= 4800) {
+                $("#reservation div.food-images").animate({ opacity: 1 }, 1000);
+            }
+        }
+        else {
+            //Images Animation (Mobile)
+            if ($(window).scrollTop() >= 80) {
                 $("header figure.burger-image").animate({ left: "0%", opacity: 1 }, 300);
             }
             if ($(window).scrollTop() >= 300) {
@@ -197,3 +119,96 @@ $(function () {
     });
     
 });
+
+function WhiteHover() {
+    $("header nav a").css({ color: "rgb(61, 37, 20)" });
+    $("header nav a").mouseenter(function () {
+        $(this).css({ color: "#ffffff" });
+    });
+    $("header nav a").mouseleave(function () {
+        $(this).css({ color: "rgb(61, 37, 20)" });
+    });
+}
+
+function WhiteHover2() {
+    $("header nav a").css({ color: "#000000" });
+    $("header nav a").mouseenter(function () {
+        $(this).css({ color: "#ffffff" });
+    });
+    $("header nav a").mouseleave(function () {
+        $(this).css({ color: "#000000" });
+    });
+}
+
+function GrayHover() {
+    $("header nav a").css({ color: "#ffffff" });
+    $("header nav a").mouseenter(function () {
+        $(this).css({ color: "darkgray" });
+    });
+    $("header nav a").mouseleave(function () {
+        $(this).css({ color: "#ffffff" });
+    });
+}
+
+var isMobile = false;
+var isTablet = false;
+var isLaptop = false;
+function CheckDeviceWidth() {
+    isMobile = false;
+    isTablet = false;
+    isLaptop = false;
+    if ($(window).width() < 768) {
+        isMobile = true;
+    }
+    else if ($(window).width() >= 768 && $(window).width() <= 991) {
+        isTablet = true;
+    }
+    else {
+        isLaptop = true;
+    }
+}
+
+function NavbarFormat() {
+    if (isMobile) {
+        $(".navigation").addClass("navbar-fixed-top");
+        GrayHover();
+    }
+    else if (isTablet) {
+        $(".navigation").removeClass("navbar-fixed-top");
+        FixNavbar();
+    }
+    else {
+        $(".navigation").removeClass("navbar-fixed-top");
+        if ($(window).scrollTop() >= 5) {
+            FixNavbar();
+        }
+    }
+}
+
+function FixNavbar() {
+    $("header nav").css({
+        position: "fixed",
+        left: 0,
+        top: -22,
+        backgroundColor: "darkgray",
+        opacity: 0.5,
+        padding: "7px",
+        width: "100%",
+        zIndex: 3
+    });
+    WhiteHover2();
+}
+
+function FreeNavbar() {
+    $("header nav").css({
+        width: "auto",
+        background: "none",
+        opacity: 1,
+        marginTop: "21px",
+        marginRight: "46px",
+        position: "relative",
+        left: 0,
+        top: 0
+    });
+    WhiteHover();
+}
