9919dfb23d491f46818c08e15af5d7b7ce4c3a66
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-10-27 09:05:43 +0400

    Commit!

--- Csharp_practise/Csharp_practise/StringFormat.cs
+++ Csharp_practise/Csharp_practise/StringFormat.cs
@@ -8,7 +8,7 @@ namespace Csharp_practise
     {
         public void Run()
         {
-            decimal price = 2500.0m, discount = 0.2m;
+            decimal price = 2500.0M, discount = 0.2M; //M is refering to "Money"
             Console.WriteLine($"Original price is {price} OMR");
             Console.WriteLine("Discount is {0}", (discount * 100) + "%");
             Console.WriteLine("Price after discount is " + (price - (price * discount)) + " OMR");
