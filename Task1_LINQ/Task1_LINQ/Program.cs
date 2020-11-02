478ee2fecffa7eb7e3f3e5ac9354dbbd3681f394
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-11-02 11:36:03 +0400

    *****

---
+++ Task1_LINQ/Task1_LINQ/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var users = User.GetUsers();
+            //int n = 0, t = 4;
+            //var records = users.Skip(n).Take(t).ToList();
+            //int numOfIteration = int.Parse(Math.Ceiling(users.Count / float.Parse(t.ToString())).ToString());
+            //for (int i = 0; i < numOfIteration; i++)
+            //{
+            //    foreach (var item in records)
+            //    {
+            //        Console.WriteLine("ID: {0}\tEmail: {1}\tPassword: {2}\tAddress: {3}\tPhone: {4}", item.ID, item.Email, item.Password, item.Address, item.Phone);
+            //    }
+            //    n += t;
+            //    records = users.Skip(n).Take(t).ToList();
+            //    Console.WriteLine("");
+            //}
+
+            //var users = User.GetUsers();
+            //int n = 0, t = 4;
+            //var records = users.Skip(n).Take(t).ToList();
+            //while (true)
+            //{
+            //    if (records.Count == 0)
+            //        break;
+            //    foreach (var item in records)
+            //    {
+            //        Console.WriteLine("ID: {0}\tEmail: {1}\tPassword: {2}\tAddress: {3}\tPhone: {4}", item.ID, item.Email, item.Password, item.Address, item.Phone);
+            //    }
+            //    n += t;
+            //    records = users.Skip(n).Take(t).ToList();
+            //    Console.WriteLine("");
+            //}
+
+            //Best Algorithm
+
+        }
+    }
+}
