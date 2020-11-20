5273ff418fbfa989846d0fac05dae041c14d767c
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-11-20 17:09:17 +0400

    Update Functions.sql

--- SQL Scripts/Functions.sql
+++ SQL Scripts/Functions.sql
@@ -5,6 +5,7 @@ USE dbName
 GO
 
 NEWID() -- Generate UNIQUEIDENTIFIER
+UniqueColumn UNIQUEIDENTIFIER DEFAULT NEWID(),
 
 -- Variable Declaration
 DECLARE @mysearch NVARCHAR(60);
@@ -16,7 +17,7 @@ DECLARE
   @v2 AS INT = 50,
   @v3 AS INT = 100
 
--- OFFSET "start from 51"
+-- Fitch "start from 51"
 SELECT orderid,custid,empid,orderdate
 FROM sales.orders
 ORDER BY orderdate, orderid
@@ -34,7 +35,7 @@ VALUES (s.productname,s.supplierid,s.categoryid,s.unitprice,s.discontinued);
 
 -- RANK
 SELECT top(5) productid,productname,unitprice,
-RANK() over(ORDER BY unitprice Desc) AS rankbyprice
+RANK() OVER(ORDER BY unitprice Desc) AS rankbyprice
 FROM Production.Products
 
 -- SWITCH CASE
@@ -44,6 +45,11 @@ CASE discontinued
  WHEN 1 THEN 'NONACTIVE'
  END AS Status
 FROM Production.Products
+
+-- Pivot
+SELECT UserID, [2017], [2018], [2019], [2020]
+FROM (SELECT UserID, Events_Name, Year FROM dbo.Events) AS e
+PIVOT (COUNT(Events_Name) FOR Year IN ([2017],[2018],[2019],[2020])) AS pvt
 
 
 ISNUMERIC('1E3')
@@ -61,3 +67,9 @@ DECLARE
 NULLIF(col1,col2) -- if both are equal return null else return col2 value
 
 WHERE id <> 3 -- exclude number 3
+
+WHERE EXISTS (
+)
+
+WITH CTE AS (
+)
