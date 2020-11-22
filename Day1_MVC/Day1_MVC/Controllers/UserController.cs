ffcf90f4256aca771e26e9c4ab82886362809fbf
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-11-22 22:12:51 +0400

    ***

---
+++ Day1_MVC/Day1_MVC/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day1_MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Display()
+        {
+            ViewBag.Im = "I'm Human";
+            return View();
+        }
+
+        public ActionResult UserProfile()
+        {
+            ViewBag.Name = "Eyad";
+            ViewBag.Age = 1;
+            return View();
+        }
+    }
+}
