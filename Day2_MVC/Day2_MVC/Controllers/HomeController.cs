eb8e82ccaedcf722bc18fecc3174ec25cda4de29
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-11-23 15:14:13 +0400

    ***

---
+++ Day2_MVC/Day2_MVC/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Day2_MVC.Models; //To access classes in the Models
+
+namespace Day2_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var human = Human.GetHumen();
+            return View(human);
+        }
+    }
+}
---
+++ Day2_MVC/Day2_MVC/Models/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Day2_MVC.Models
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public static List<Human> GetHumen()
+        {
+            return new List<Human>()
+            {
+                new Human(){Name="Moayad", Age=23},
+                new Human(){Name="Mohanned", Age=21},
+                new Human(){Name="Mohammed", Age=19},
+            };
+        }
+    }
+}
