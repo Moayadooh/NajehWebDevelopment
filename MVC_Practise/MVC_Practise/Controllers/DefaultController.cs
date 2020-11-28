f3f4bc1ddc8a8e2041bd7841629ddf4986928723
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-11-28 11:05:42 +0400

    ***

---
+++ MVC_Practise/MVC_Practise/Controllers/DefaultController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Practise.Models;
+
+namespace MVC_Practise.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        CollegeDB DB = new CollegeDB();
+        public ActionResult Index()
+        {
+            GetMapPath();
+            return View(DB.Students.ToList());
+        }
+
+        public ActionResult Search(float? Score)
+        {
+            GetMapPath();
+            var model = DB.Students.ToList();
+            if (Score != null)
+                model = model.Where(x => x.Score >= Score).ToList();
+            return View(model);
+        }
+
+        public void GetMapPath()
+        {
+            ViewBag.controller = RouteData.Values["Controller"];
+            ViewBag.action = RouteData.Values["Action"];
+            ViewBag.id = RouteData.Values["id"];
+        }
+    }
+}
---
+++ MVC_Practise/MVC_Practise/Models/CollegeDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Practise.Models
+{
+    public class CollegeDB : DbContext
+    {
+        public CollegeDB() : base("constr") { }
+        public DbSet<Student> Students { get; set; }
+    }
+}
