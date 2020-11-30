fe8fee3bf7e294e57f5d91307531644c11a373a2
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-11-30 18:46:10 +0400

    ***

---
+++ Day5_MVC/Day5_MVC/Models/NajehDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Day5_MVC.Models
+{
+    public class NajehDB : DbContext
+    {
+        public NajehDB() : base("constr") { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Day5_MVC/Day5_MVC/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Day5_MVC.Models
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [RegularExpression(@"[a-zA-Z0-9_\.+]+@(live|hotmail|gmail)(\.com|\.om)",
+            ErrorMessage = "Must be live, hotmail or gmail")]
+        [Index(IsUnique = true)]
+        [MaxLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(20,MinimumLength = 3)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")] // refer to the Password field
+        public string Confirm { get; set; }
+    }
+}
