68b58f7d13a44414735d35baf4357595f7bec8c4
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-12-01 08:34:42 +0400

    ***

--- Day5_MVC/Day5_MVC/Controllers/UserController.cs
+++ Day5_MVC/Day5_MVC/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Day5_MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,86 @@ namespace Day5_MVC.Controllers
 {
     public class UserController : Controller
     {
-        // GET: User
-        public ActionResult Index()
+        NajehDB DB = new NajehDB();
+
+        [HttpGet]
+        public ActionResult Register()
         {
+            if (Session["info"] != null)
+                return RedirectToAction("Welcome");
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User user)
+        {
+            try
+            {
+                if (ModelState.IsValid) // check all fields validations
+                {
+                    DB.Users.Add(user);
+                    DB.SaveChanges();
+                    return RedirectToAction("Login");
+
+                    //if (!IsUserExist(user.Email))
+                    //{
+                    //    DB.Users.Add(user);
+                    //    DB.SaveChanges();
+                    //    return RedirectToAction("Login");
+                    //}
+                    //else
+                    //    ModelState.AddModelError("", "Email is already taken, try another one!");
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Email is already taken, try another one!");
+            }
+            return View();
+        }
+
+        //public bool IsUserExist(string email)
+        //{
+        //    return DB.Users.Count(x => x.Email == email) > 0;
+        //}
+
+        //[HttpGet] no need to type [HttpGet] if [HttpPost] is there or vice versa
+        public ActionResult Login()
+        {
+            if (Session["info"] != null)
+                return RedirectToAction("Welcome");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            //if (ModelState.IsValidField("Email") && ModelState.IsValidField("Password"))
+            //{
+                var model = DB.Users.SingleOrDefault(x => x.Email == user.Email && x.Password == user.Password);
+                if (model == null)
+                    ViewBag.errmsg = "Please Verify Email and Password";
+                else
+                {
+                    Session["info"] = user.Email;
+                    return RedirectToAction("Welcome");
+                }
+            //}
+            return View();
+        }
+
+        public ActionResult Welcome()
+        {
+            //if (Session["info"] == null)
+            //    return RedirectToAction("Login");
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session["info"] = null;
+            return RedirectToAction("Login");
         }
     }
 }
