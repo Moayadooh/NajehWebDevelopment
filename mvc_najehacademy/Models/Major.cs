42502c380f1ccbde36dada04d21de32287faa0dd
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-12-04 12:12:14 +0400

    ***

---
+++ mvc_najehacademy/Models/Major.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc_najehacademy.Models
+{
+    public class Major
+    {
+        public int ID { get; set; } // PK
+
+        public string Title { get; set; }
+
+        public virtual List<User> Users { get; set; }
+    }
+}
---
+++ mvc_najehacademy/Models/NajehDB.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mvc_najehacademy.Models
+{
+    public class NajehDB : DbContext
+    {
+        public NajehDB() : base("constr") { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Major> Majors { get; set; }
+    }
+}
