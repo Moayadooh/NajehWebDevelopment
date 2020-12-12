9a416fc609d0a582376dd7514a9cb2f0be18850c
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-12-12 21:07:12 +0400

    ***

---
+++ Exam 70-486/Exam 70-486/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Exam_70_486.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exam_70_486.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Data = new DateAndTime().ShortDate;
+            return View();
+        }
+    }
+}
---
+++ Exam 70-486/Exam 70-486/Models/DateAndTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exam_70_486.Models
+{
+    public class DateAndTime
+    {
+        public DateTime dateTime = DateTime.Now;
+        public string ShortDate
+        {
+            get
+            {
+                return dateTime.ToLocalTime().ToShortDateString();
+                //return dateTime.ToLocalTime().ToString();
+            }
+        }
+    }
+}
