8b8680803e66a2d8c873a9b5458e9905656ebcb3
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2020-12-20 20:54:36 +0400

    ***

---
+++ SignalR/SignalR/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Chat()
+        {
+            return View();
+        }
+
+        public ActionResult SendNotify()
+        {
+            return View();
+        }
+
+        public ActionResult Employee()
+        {
+            return View();
+        }
+
+        public ActionResult Manager()
+        {
+            return View();
+        }
+    }
+}
---
+++ SignalR/SignalR/Hubs/NotificationHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public void NotifyAll(string title, string message, string alerttype) // send from SendNotify CSHTML
+        {
+            Clients.All.displayNotification(title, message, alerttype); // on layout
+        }
+    }
+}
