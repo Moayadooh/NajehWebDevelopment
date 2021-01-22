01730eb372fa4b403077b3e75bef101e324722fe
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2021-01-22 20:37:04 +0400

    ***

---
+++ WCF/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using ServiceReferenceCustomerMoayad1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ServiceClient srv = new ServiceClient();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = srv.GetAllCustomerAsync();
+        }
+    }
+}
