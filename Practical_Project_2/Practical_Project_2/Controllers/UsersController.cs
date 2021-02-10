7b053c60f2689543e094d0272c0511dd5f59c009
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2021-02-10 17:40:58 +0400

    ***

--- Practical_Project_2/Practical_Project_2/Controllers/UsersController.cs
+++ Practical_Project_2/Practical_Project_2/Controllers/UsersController.cs
@@ -17,65 +17,32 @@ namespace Practical_Project_2.Controllers
     {
         private ShoppingDB db = new ShoppingDB("");
 
-        // (GET request)
-        // get users and purchased items for each user
-        [Route("api/GetUsersItemsPurchases")]
-        public IQueryable<UsersItemsPurchases> GetUsersItemsPurchases()
+        public string GetRoleName(List<UserRole> roleList, User user)
         {
-            var list = db.Users.Include(x => x.UserPurchases).Include(y => y.Profile).ToList();
-            var usersItemsPurchases = new List<UsersItemsPurchases>();
-            foreach (var item in list)
-            {
-                if (item.UserPurchases.Count == 0)
-                    usersItemsPurchases.Add(new UsersItemsPurchases() { FirstName = item.Profile.FirstName, LastName = item.Profile.LastName, Email = item.Email, NumOfItems = "-", TotalPrice = "-", Date = "-" });
-                foreach (var userPurchasesItem in item.UserPurchases)
-                {
-                    usersItemsPurchases.Add(new UsersItemsPurchases() { FirstName = item.Profile.FirstName, LastName = item.Profile.LastName, Email = item.Email, NumOfItems = userPurchasesItem.NumOfItems.ToString(), TotalPrice = userPurchasesItem.TotalPrice.ToString(), Date = userPurchasesItem.Date.ToString("MM/dd/yyyy hh:mm tt") });
-                }
-            }
-            return usersItemsPurchases.AsQueryable();
-
-            //var records = usersItemsPurchases.ToList();
-            //switch (sortColumn)
-            //{
-            //    case "FirstName":
-            //        records = records.OrderBy(x => x.FirstName).ToList();
-            //        break;
-            //    case "LastName":
-            //        records = records.OrderBy(x => x.LastName).ToList();
-            //        break;
-            //    case "Email":
-            //        records = records.OrderBy(x => x.Email).ToList();
-            //        break;
-            //    case "NumOfItems":
-            //        records = records.OrderBy(x => x.NumOfItems).ToList();
-            //        break;
-            //    case "TotalPrice":
-            //        records = records.OrderBy(x => x.TotalPrice).ToList();
-            //        break;
-            //    case "Date":
-            //        records = records.OrderBy(x => x.Date).ToList();
-            //        break;
-            //    default:
-            //        break;
-            //}
-
-            //if (!string.IsNullOrEmpty(filter1))
-            //{
-            //    filter1 = filter1.ToLower();
-            //    records = records.Where(x => x.FirstName.ToLower().Contains(filter1) || x.LastName.ToLower().Contains(filter1) || x.Email.ToLower().Contains(filter1) || x.NumOfItems.ToLower().Contains(filter1) || x.TotalPrice.ToLower().Contains(filter1) || x.Date.ToLower().Contains(filter1)).ToList();
-            //}
-
-            //if (itemsPerPage == -1)
-            //    records = records.ToList(); // get all items
-            //else
-            //{
-            //    int itemsToSkip = pageNum * itemsPerPage - itemsPerPage;
-            //    records = records.Skip(itemsToSkip).Take(itemsPerPage).ToList();
-            //}
-
+            var list1 = roleList.Where(x => x.UserID == user.ID).ToList();
+            int roleID = list1.FirstOrDefault().RoleID;
+            var list2 = roleList.Where(x => x.Role.ID == roleID).ToList();
+            var roleName = list2.FirstOrDefault().Role.Name;
+            return roleName;
         }
 
+        // (GET request)
+        // get users details
+        [Route("api/GetUserDetails")]
+        public IQueryable<UserDetails> GetUserDetails()
+        {
+            var list = db.Users.Include(x => x.Profile).Include(y => y.UserRoles).ToList();
+            var roleList = db.UserRoles.Include(x => x.Role).ToList();
+
+            var userDetails = new List<UserDetails>();
+            foreach (var item in list)
+            {
+                var roleName = GetRoleName(roleList, item);
+                userDetails.Add(new UserDetails() { ID = item.ID, FirstName = item.Profile.FirstName, LastName = item.Profile.LastName, Email = item.Email, Role = roleName });
+            }
+            return userDetails.AsQueryable();
+        }
+        //NumOfItems = userPurchasesItem.NumOfItems.ToString(), TotalPrice = userPurchasesItem.TotalPrice.ToString(), Date = userPurchasesItem.Date.ToString("MM/dd/yyyy hh:mm tt")
         protected override void Dispose(bool disposing)
         {
             if (disposing)
---
+++ Practical_Project_2/Practical_Project_2/ViewModel/UserDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practical_Project_2.ViewModel
+{
+    public class UserDetails
+    {
+        [Required]
+        public Guid ID { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+}
