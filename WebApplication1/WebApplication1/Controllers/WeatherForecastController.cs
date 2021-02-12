3a4b8c2d06bd73df02a5a092a32af953532a6b25
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2021-02-12 19:27:14 +0400

    ***

---
+++ WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.MyClasses;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public Course course = new Course();
+
+        [HttpGet]
+        public IActionResult Read()
+        {
+
+            if (!course.isInitialized)
+            {
+                course.Initial();
+                course.isInitialized = true;
+            }
+            return Ok(course.Read());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetbyId(int id)
+        {
+            return Ok(course.GetById(id));
+        }
+
+        [HttpPost("{courseCode}/{courseName}/{date}")]
+        public IActionResult Create(string courseCode, string courseName, string date)
+        {
+            course.Create(courseCode, courseName, date);
+            return Ok();
+        }
+
+        [HttpPut("{id}/{courseCode}/{courseName}/{date}")]
+        public IActionResult Edit(int id, string courseCode, string courseName, string date)
+        {
+            course.Edit(id, courseCode, courseName, date);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            course.Delete(id);
+            return Ok();
+        }
+
+
+
+        [HttpGet]
+        public IActionResult GetName()
+        {
+            return Ok("Moayad");
+        }
+
+        [HttpGet("{age}")]
+        public IActionResult GetAge(int age)
+        {
+            return Ok(age);
+        }
+
+        //private static readonly string[] Summaries = new[]
+        //{
+        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //};
+
+        //private readonly ILogger<WeatherForecastController> _logger;
+
+        //public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+    }
+}
---
+++ WebApplication1/WebApplication1/MyClasses/Course.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.MyClasses
+{
+    public class Course
+    {
+        public int ID { get; set; }
+
+        public string CourseCode { get; set; }
+
+        public string CourseName { get; set; }
+
+        public DateTime Date { get; set; }
+
+        List<Course> Courses;
+        int i;
+        public bool isInitialized;
+        public Course()
+        {
+            Courses = new List<Course>();
+            i = 4;
+            isInitialized = false;
+            //Courses = new List<Course>
+            //{
+            //    new Course { ID = 1, CouseCode = "CS101", CouseName = "Introduction to Computer Science", Date = DateTime.Parse("2021-03-01 15:00:00")},
+            //    new Course { ID = 2, CouseCode = "CS102", CouseName = "Algorithm I", Date = DateTime.Parse("2021-03-01 08:00:00")},
+            //    new Course { ID = 3, CouseCode = "CS201", CouseName = "Introduction To DBMS", Date = DateTime.Parse("2021-03-01 12:30:00")}
+            //};
+        }
+
+        public void Initial()
+        {
+            Courses.Add(new Course { ID = 1, CourseCode = "CS101", CourseName = "Introduction to Computer Science", Date = DateTime.Parse("2021-03-01 15:00:00") });
+            Courses.Add(new Course { ID = 2, CourseCode = "CS102", CourseName = "Algorithm I", Date = DateTime.Parse("2021-03-01 08:00:00") });
+            Courses.Add(new Course { ID = 3, CourseCode = "CS201", CourseName = "Introduction To DBMS", Date = DateTime.Parse("2021-03-01 12:30:00") });
+        }
+
+        public List<Course> Read()
+        {
+            return Courses;
+        }
+
+        public Course GetById(int id)
+        {
+            return Courses.SingleOrDefault(x => x.ID == id);
+        }
+
+        public void Create(string couseCode, string couseName, string date)
+        {
+            Courses.Add(new Course { ID = i++, CourseCode = couseCode, CourseName = couseName, Date = DateTime.Parse(date) });
+        }
+
+        public void Edit(int id, string couseCode, string couseName, string date)
+        {
+            Courses.Remove(Courses.Single(x => x.ID == id));
+            Courses.Add(new Course { ID = id, CourseCode = couseCode, CourseName = couseName, Date = DateTime.Parse(date) });
+        }
+
+        public void Delete(int id)
+        {
+            Courses.Remove(Courses.Single(x => x.ID == id));
+        }
+    }
+}
