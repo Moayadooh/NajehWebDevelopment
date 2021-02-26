8924fc1950a80a97590ec213464d8b039d89af47
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2021-02-26 22:07:41 +0400

    ***

---
+++ Mine/WebAPI_JWT/WebAPI_JWT/Controllers/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebAPI_JWT.Controllers
+{
+    internal class UserModel
+    {
+        public string Username { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public DateTime DateOfJoing { get; set; }
+    }
+}
