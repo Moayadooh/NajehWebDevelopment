55dc883f0bf5086db8c3c9d1761ec30437480b5b
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2021-04-02 10:17:47 +0400

    ***

---
+++ Csharp/ConsoleAppTest/ConsoleAppTest/ClassA.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppTest
+{
+    public class ClassA
+    {
+        public String name;
+    }
+}
--- Csharp/ConsoleAppTest/ConsoleAppTest/Program.cs
+++ Csharp/ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -6,16 +6,17 @@ namespace ConsoleAppTest
     {
         static void Main(string[] args)
         {
-            decimal salary = 300.0M; //Original salary
-            int delay = 11; //Number of days
-            decimal punishment = 0.0M;
+            var a = new ClassA();
+            var b = a;
 
-            for (int i = 1; i <= delay; i++)
-            {
-                punishment += salary * (i * .01M);
-            }
-            decimal newSalary = salary + punishment;
-            Console.WriteLine($"Employee must be given {newSalary} OMR");
+            a.name = "Eyad";
+            Console.WriteLine(b.name);
+
+            int n = 5;
+            int o = n;
+            n = 8;
+            Console.WriteLine(o);
+
         }
     }
 }
