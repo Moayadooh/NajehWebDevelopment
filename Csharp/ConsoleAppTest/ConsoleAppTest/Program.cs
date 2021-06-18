d2e7190429f77cfa3e64b294cc19a16e34a4c009
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2021-06-18 22:56:05 +0400

    ***

--- Csharp/ConsoleAppTest/ConsoleAppTest/Program.cs
+++ Csharp/ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace ConsoleAppTest
 {
@@ -6,16 +9,6 @@ namespace ConsoleAppTest
     {
         static void Main(string[] args)
         {
-            var a = new ClassA();
-            var b = a;
-
-            a.name = "Eyad";
-            Console.WriteLine(b.name);
-
-            int n = 5;
-            int o = n;
-            n = 8;
-            Console.WriteLine(o);
 
         }
     }
