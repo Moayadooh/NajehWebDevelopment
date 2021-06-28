e422619dfc99874fc81455a1fa8c2d66128d1ca1
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2021-06-28 14:57:31 +0400

    ***

--- SQL/SQLScripts3/SQLScripts3/SQLQuery8.sql
+++ SQL/SQLScripts3/SQLScripts3/SQLQuery8.sql
@@ -6,3 +6,8 @@ update set p.discontinued = s.discontinued
 when not matched then
 insert ([productname],[supplierid],[categoryid],[unitprice],[discontinued])
 values (s.productname,s.supplierid,s.categoryid,s.unitprice,s.discontinued);
+
+select * from Production.Products
+select * from Production.ProductsStaging
+
+update Production.ProductsStaging set discontinued = 0
